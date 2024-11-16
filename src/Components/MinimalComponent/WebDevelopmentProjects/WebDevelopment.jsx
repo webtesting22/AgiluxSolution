1678fd5438485e48266ded9634b9d49e46798221
webtesting22 <webtesting2232@gmail.com>
2024-11-16 09:39:56 +0530

    Image Updated

--- src/Components/MinimalComponent/WebDevelopmentProjects/WebDevelopment.jsx
+++ src/Components/MinimalComponent/WebDevelopmentProjects/WebDevelopment.jsx
@@ -6,17 +6,17 @@ const WebDevelopment = () => {
     const WebDevelopmentProjetsData = [
         {
             title: "Structura",
-            image:Structura,
+            image: Structura,
             desctiption: "With experience in over 1500 diverse projects, Strutura has established itself as a leading guide in the construction industry. We excel in providing comprehensive solutions across BIM, interior design, engineering, architecture, and structural planning, alongside cutting-edge 3D visualization services. Strutura positions itself as a navigator, helping clients maximize business value and achieve their project goals with precision and expertise."
         },
         {
             title: "Yellowspot",
-            image:Yellowspot,
+            image: Yellowspot,
             desctiption: "Yellow Spot was born from the strategic collaboration between Het Graphics and Khushi Advertising, uniting two industry leaders to create one of Gujarat's premier outdoor media houses. With a portfolio of over 400 premium outdoor media options across Ahmedabad, 99% of which are backlit, we ensure maximum visibility in high-traffic locations and every neighborhood throughout the city. Our mission is to deliver comprehensive 360-degree marketing solutions, catering to local, national, and international clients across a diverse range of platforms. At Yellow Spot, we are dedicated to providing impactful and effective advertising solutions that drive results."
         },
         {
             title: "Aaryaneev",
-            image:Aaryaneev,
+            image: Aaryaneev,
             desctiption: "Aaryarath began its journey over 30 years ago with a modest vision, leveraging family support to fund its first major project. The success was immediate, with 100% of offerings sold and an overwhelming response that set the foundation for future ventures. With strategic planning and careful execution, Aaryarath steadily expanded its portfolio. To fuel this growth, capital was raised, and the relentless efforts of Mr. Ratilal, a key driving force, helped propel the business forward. Today, Aaryarath stands as a multi-generational brand, proudly carrying its legacy of creating iconic landmarks."
         },
 
@@ -44,7 +44,7 @@ const WebDevelopment = () => {
                                     data-aos-duration="1000"
                                     data-aos-delay={`${index * 100}`}>
                                     <div className="ProjectimageContiner">
-                                            <img src={item.image} style={{width:"100%"}} alt="" />
+                                        <img src={item.image} style={{ width: "100%" }} alt="Agilux Solutions company logo" />
                                     </div>
                                     <p>/ {item.title}</p>
                                     <br />
